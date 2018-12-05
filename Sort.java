05a8d3b1658ee506c2f3154462bf0f9ca86e2cc6
qtgeo1248 <gzhou00@stuy.edu>
2018-12-04 21:22:23 -0500

    made first for loop

--- Sort.java
+++ Sort.java
@@ -1,6 +1,10 @@
 public class Sort {
     public static void selectionsort(int[] ary) {
-        
+        for (int i = 0; i < ary.length; i++) {
+            int smallest = ary[i];
+            int index = i;
+            
+        }
     }
     public static void main(String[] args) {
     }
