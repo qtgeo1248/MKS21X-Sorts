7f1a9857e623ecefc7d9d1043170096854312302
qtgeo1248 <gzhou00@stuy.edu>
2018-12-05 18:14:05 -0500

    added print stuff

--- Sorts.java
+++ Sorts.java
@@ -14,6 +14,7 @@ public class Sorts {
             }
             ary[smallIdx] = ary[i];
             ary[i] = smallest; //do a swap swap thingy
+            System.out.println(Arrays.toString(ary));
         }
     }
     public static void main(String[] args) {
@@ -27,6 +28,7 @@ public class Sorts {
                 System.exit(1);
             }
         }
+        System.out.println();
 
         int[] test2 = new int[0]; //ans = []
         try {
@@ -35,6 +37,7 @@ public class Sorts {
             System.out.println("aiyahh you failed test2");
             System.exit(1);
         }
+        System.out.println();
 
         int[] test3 = {7, 3, 9, 1, 6, 0, 4, 8, 2, 5, 6, 2, 4, 1, 9, 0, 3, 7, 8, 5};
         selectionSort(test3); //ans = [0,0,1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9]
@@ -44,6 +47,16 @@ public class Sorts {
                 System.exit(1);
             }
         }
+        System.out.println();
+
+        int[] Ktest1 = {7, 5, 4, 2};
+        selectionSort(Ktest1);
+        System.out.println();
+        int[] Ktest2 = {5, 1, 12, -5, 16, 2, 12, 14};
+        selectionSort(Ktest2);
+        System.out.println();
+
+        /*
         Random rng = new Random();
         for (int idx = 0; idx < 10000; idx++) {//tests 10000 times
             int[] key = new int[1000]; //each with 1000 elements :)))
@@ -66,5 +79,6 @@ public class Sorts {
             }
         }
         System.out.println("How many wrongs: " + totalWrongs);
+        */
     }
 }
