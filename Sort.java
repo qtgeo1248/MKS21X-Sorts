ad00aed6e3caaa78de5920dfcaa3e88a79fd5858
qtgeo1248 <gzhou00@stuy.edu>
2018-12-04 21:23:36 -0500

    made both loops

--- Sort.java
+++ Sort.java
@@ -2,8 +2,13 @@ public class Sort {
     public static void selectionsort(int[] ary) {
         for (int i = 0; i < ary.length; i++) {
             int smallest = ary[i];
-            int index = i;
-            
+            int smallIdx = i;
+            for (int j = i; j < ary.length; j++) {
+                if (ary[j] < smallest) {
+                    smallest = ary[j];
+                    smallIdx = j;
+                }
+            }
         }
     }
     public static void main(String[] args) {
