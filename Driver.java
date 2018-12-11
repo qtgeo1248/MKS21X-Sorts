26b470f182bedc168648ae5f2ab98eca8c1da6ed
George Zhou <gzhou00@stuy.edu>
2018-12-11 14:52:15 -0500

    added Driver

---
+++ Driver.java
@@ -0,0 +1,42 @@
+import java.util.Arrays;
+public class Driver {
+    public static void main(String[] artie) {
+        int[] randish = new int[Integer.parseInt(artie[0])];
+        for (int i = 0 ; i < randish.length; i++) {
+            randish[i] =(int)(Math.random() * 10000);
+        }
+        if (artie[1].equals("selection")) {
+            Sorts.selectionSort(randish);
+        }
+        if (artie[1].equals("bubble")) {
+            Sorts.bubbleSort(randish);
+        }
+        if (artie[1].equals("insertion")) {
+            Sorts.insertionSort(randish);
+        }
+        if (artie[1].equals("test")) {
+            int[] randish2 = Arrays.copyOf(randish, randish.length);
+            int[] randish3 = Arrays.copyOf(randish, randish.length);
+            int[] randish4 = Arrays.copyOf(randish, randish.length);
+            Sorts.selectionSort(randish);
+            Sorts.bubbleSort(randish2);
+            Sorts.insertionSort(randish4);
+            Arrays.sort(randish3);
+            if (Arrays.equals(randish, randish3)) {
+                System.out.println("Selection Correct!");
+            } else {
+                System.out.println("Selection BROKEN!!!!");
+            }
+            if (Arrays.equals(randish2, randish3)){
+                System.out.println("Bubble Correct!");
+            } else {
+                System.out.println("Bubble BROKEN!!!!");
+            }
+            if (Arrays.equals(randish4, randish3)){
+                System.out.println("Insertion Correct!");
+            } else {
+                System.out.println("Insertion BROKEN!!!!");
+            }
+        }
+    }
+}
